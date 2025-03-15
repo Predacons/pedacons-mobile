ba169ca5a3ba5cf9a0e3d9603b8c6bf392244570
Shourya Shashank <shouryashashank@gmail.com>
2025-03-15 15:15:45 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -14,7 +14,7 @@ class MainApp(Control):
         super().__init__()
         self.page = page
         self.page.theme.use_material3 = True
-        self.game_list = ListView(expand=1, spacing=10, padding=20, auto_scroll=False)  # Initialize game_list as a scrollable Column
+        self.chat_list = ListView(expand=1, spacing=10, padding=20, auto_scroll=False)  # Initialize chat_list as a scrollable Column
         # self.user_id_field = TextField(label="User ID")
         self.page.floating_action_button = FloatingActionButton(
                 icon=Icons.ADD,
@@ -22,8 +22,8 @@ class MainApp(Control):
                 on_click=self.open_new_page,
             )
         self.setup_ui()
-        # Load games when the app starts
-        self.load_games(None)
+        # Load chats when the app starts
+        self.load_chats(None)
         if ENABLE_ADS and self.page.platform != PagePlatform.WINDOWS:
             self.setup_ads()
     def _get_control_name(self):
@@ -42,121 +42,41 @@ class MainApp(Control):
         
     def setup_ui(self):
         self.page.add(self.app_bar())
-        self.page.add(
-            SafeArea(
-                content=Column([
-
-                    
-                    # self.user_id_field,
-                    ElevatedButton(text="Load Games", on_click=self.reload_games),
-                                     
-                
-                ])
-            )
-        )
-        self.page.add(self.game_list)
+        self.page.add(self.chat_list)
         
     
     def open_new_page(self, e):
         self.page.go('/add')
     
-    def open_game_page(self,game_id):
-        self.page.go('/game/'+str(game_id))
+    def open_chat_page(self,chat_id):
+        self.page.go('/chat/'+str(chat_id))
 
-    def reload_games(self, e):
-        try:
-            db = Database()
-            db.connect_to_db()
-            user_id = db.read_user_db()[0].steamid
-            # Get user's game list
-            games_data = self.get_users_game_list(user_id)
-            
-            # Get game objects from games_data
-            games = []
-            for game_info in games_data:
-                for game_id, game_details in game_info.items():
-                    if game_details['success']:
-                        game = steam_to_game(game_details['data'])
-                        games.append(game)
-        
-            # Save games to database
-            
-            db.insert_multiple_games(games)
-            db.close_db()
+    def load_chats(self, e):
 
-            self.load_games(None)
-            print("Games reloaded")
-        except Exception as ex:
-            self.page.add(Text(f"Error: {str(ex)}", color="red"))
-            print(f"Error: {str(ex)}")
-
-    def load_games(self, e):
-        # with open("games.json", "r", encoding="utf-8") as f:
-        #     games_data = json.load(f)
         db = Database()
         db.connect_to_db()
         db_path = db.return_db_path()
         self.page.add(Text(db_path, color="red"))
-        games_data = db.read_game_db()
+        chats_data = db.read_chat_db()
         db.close_db()
-        self.game_list.controls.clear()
-        for game in games_data:
-            header_image = game.header_image
-            name = game.name
-            play_time = game.playtime_forever
-            last_played = game.rtime_last_played
-            completed = game.completed
-            short_description = game.short_description[:100] + "..."  # Truncate description
-            website = game.link
+        self.chat_list.controls.clear()
+        add_item = ListTile(
+            leading=Image(src="https://img.icons8.com/ios/452/plus.png"),
+            title=Text("Add new chat"),
+            on_click=self.open_new_page
+        )
+        for chat in chats_data:
+            name = chat.name
+            vector_db = chat.vectordb
+            web_search = chat.websearch
             list_item = ListTile(
-                leading=Image(src=header_image),
                 title=Text(name),
-                subtitle=Text(f"Playtime: {play_time} minutes \nCompleted: {'Yes' if completed else 'No'} \nLast Played: {last_played}"),
-                on_click=lambda _, game_id=game.id: self.open_game_page(game_id)
+                subtitle=Text(f"vector db: {vector_db if vector_db else 'No'} minutes \nCompleted: {'Yes' if web_search else 'No'} "),
+                on_click=lambda _, chat_id=chat.id: self.open_chat_page(chat_id)
             )
-            self.game_list.controls.append(list_item)
+            self.chat_list.controls.append(list_item)
     
         self.page.update()
-
-
-    def save_to_json(self,games_data):
-        with open("games.json", "w") as f:
-            json.dump(games_data, f)
-        self.page.add(Text("Data saved to games.json", color="green"))
-
-    def read_from_json(self, e):
-        try:
-            with open("user_input.json", "r") as f:
-                data = json.load(f)
-            self.page.add(Text(f"Read from JSON: {data['user_input']}", color="blue"))
-        except FileNotFoundError:
-            self.page.add(Text("No data found", color="red"))
-
-    def get_users_game_list(self,user_id):
-        user_games = self.steam.users.get_owned_games(user_id)
-        game_info = []
-        for game in user_games.get("games"):
-            try:
-                info = self.steam.apps.get_app_details(game["appid"])
-                app_id = str(game["appid"])
-                fields = [
-                ("playtime_forever", 0),
-                ("playtime_windows_forever", 0),
-                ("playtime_mac_forever", 0),
-                ("playtime_linux_forever", 0),
-                ("playtime_deck_forever", 0),
-                ("rtime_last_played", 0),
-                ("completed", False),
-                ("completed_date", None),
-                ("rating", None)
-                ]
-
-                for field, default in fields:
-                    info[app_id]["data"][field] = game.get(field, default)
-                game_info.append(info)
-            except Exception as ex:
-                print(f"Error fetching game details for appid {game['appid']} {game}: {str(ex)}")
-        return game_info
 
     def setup_ads(self):
         id_banner = (
